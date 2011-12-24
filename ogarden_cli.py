dcd55fa5b5b235a81ac3aeb916a18189b657286c
Enrico Rossi <e.rossi@tecnobrain.com>
2011-12-24 13:31:09 +0100

    Added the new command line interface program to dialog with the device.

---
+++ ogarden_cli.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# Copyright (C) 2011, 2012 Enrico Rossi
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+""" OpenGarden command line interface
+
+This is the command line interface to the Open Garden device.
+"""
+__author__ = "Enrico Rossi <e.rossi@tecnobrain.com>"
+__date__ = "23 December 2011"
+__version__ = "$Revision: 0.1a $"
+__credits__ = """Nicola Galliani, the "On the field" beta tester.
+Andrea Marabini, the electronic designer.
+Alessandro Dotti Contra, GUI developer.
+"""
+
+import argparse
+import sys
+from opengarden import OpenGarden
+
+parser = argparse.ArgumentParser(description='OpenGarden CLI.')
+parser.add_argument('--get-time', action='store_true')
+parser.add_argument('--set-time', type=long)
+parser.add_argument('--sunsite', action='store_true')
+parser.add_argument('--temperature', action='store_true')
+parser.add_argument('--get-version', action='store_true')
+parser.add_argument('--device', default='/dev/ttyUSB0', required=True)
+args = parser.parse_args()
+
+og = OpenGarden()
+og.connect(args.device)
+
+if og.id is None:
+    print "No Open Garden device connected or problems!"
+    raise "no device found error"
+else:
+    print "Open garden device [" + og.id + "] found."
+
+if args.temperature:
+    temperature = og.temperature()
+    print "temperature is: " + temperature[0]
+    print "media 24h is: " + temperature[1]
+    print "dfactor is: " + temperature[2]
+
+if args.sunsite:
+    print "susite setup to: " + og.sunsite
+
+if args.set_time:
+    og.time(args.set_time)
+    print "set time to: " + str(args.set_time)
+
+if args.get_time:
+    print "OG clock is: " + og.time()
+
+#print "set the time to 123 sec. from the epoch"
+#print og.time(123)
+#
+#print "load programs from the device"
+#og.load()
+#print "programs loaded:"
+#
+#for i in og.programs:
+#    print i
+
+print "disconnecting the device"
+og.disconnect()
+del(og)
+del(parser)
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
